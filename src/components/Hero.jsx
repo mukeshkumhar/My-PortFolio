e20384a6b946a19205a8c62aef1f375cab847e80
Codewhatyouwant <mukeshkumhar906@gmail.com>
2024-06-29 19:59:25 +0530

    Mouse scroll fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className=" absolute xs:bottom-5   w-full flex justify-center items-center" style={{bottom:25}}>
+      <div className=" absolute xs:bottom-5   w-full flex justify-center items-center" style={{bottom:10}}>
         <a href="#about">
           <div className="w-[35px] h-[65px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
